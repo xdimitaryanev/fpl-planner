61a9b938e82682b8e32b402d3323f61399911f1b
xdimitaryanev <118223301+xdimitaryanev@users.noreply.github.com>
2023-08-26 15:37:18 +0300

    data fetching

--- data.js
+++ data.js
@@ -1,40 +1,64 @@
-async function getTeamInfo(id,gw) {
-    const response = await fetch(`https://fantasy.premierleague.com/api/entry/${id}/event/${gw}/picks/`)
-    return await response.json()
-}
-
-async function getPicks () {
-    const teamId = 423253;
-    const gw = 3;
-    const team = await getTeamInfo(teamId,gw);
-    return team.picks // arr of objects
-}
-
 async function getGeneralInfo() {
-    const response = await fetch(`https://fantasy.premierleague.com/api/bootstrap-static/`)
-    return await response.json()
+    const response = await fetch(`https://fantasy.premierleague.com/api/bootstrap-static/`);
+    return await response.json();
 }
 
-async function getPlayerInfo(playerId) {
-    const generalInfo = await getGeneralInfo()
-    const player = generalInfo.elements.filter(element=> element.id === playerId)
-    console.log(player[0].web_name)
+async function getTeamInfo(id,gw) {
+    const response = await fetch(`https://fantasy.premierleague.com/api/entry/${id}/event/${gw}/picks/`);
+    return await response.json();
 }
 
 async function getCurrentGw() {
-    const info = await getGeneralInfo();
-    let events = info.events
-    return events.find(event => event.finished === false);
+    const data = await getGeneralInfo();
+    let events = data.events;
+    const currentGw = events.find(event => event.is_current === true);
+    console.log(currentGw);
+    return [currentGw.id, currentGw.deadline_time];
 }
 
+async function getPicks () {
+    const userId = 423253;
+    const [currentGw, currentDeadline] = await getCurrentGw();
+    console.log(currentGw);
+    console.log(currentDeadline)
+    const user = await getTeamInfo(userId, currentGw);
+    return user.picks; // arr of user team objects
+}
+
+async function getPlayerInfo(playerId) {
+    const data = await getGeneralInfo();
+    const player = data.elements.filter(element=> element.id === playerId);
+    console.log(player[0].web_name);
+    return { ...player[0] }; // single player object
+}
 
 async function loadTeam() {
     const team = await getPicks();
     team.forEach(element => {
-        console.log(element)
-        getPlayerInfo(element.element)
+        console.log(element);
+        getPlayerInfo(element.element);
     });
 }
 
-loadTeam()
+async function getAllTeams() {
+    const data = await getGeneralInfo();
+    console.log(data.teams);
+    return data.teams // arr of teams objects
+}
+
+async function getAllPlayers() {
+    const data = await getGeneralInfo();
+    const players = data.elements;
+    return players; //arr of players objects
+}
+
+async function getFixturesOfPlayer() {
+    //todo
+}
+
+getAllPlayers()
+
+loadTeam();
+
+getAllTeams()
 
