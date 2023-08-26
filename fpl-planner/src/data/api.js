90992e561d3335c272d6287f447d04480fff0ed1
xdimitaryanev <118223301+xdimitaryanev@users.noreply.github.com>
2023-08-27 01:32:23 +0300

    update

--- fpl-planner/src/data/api.js
+++ fpl-planner/src/data/api.js
@@ -12,24 +12,32 @@ async function getCurrentGw() {
     const data = await getGeneralInfo();
     let events = data.events;
     const currentGw = events.find(event => event.is_current === true);
-    console.log(currentGw);
     return [currentGw.id, currentGw.deadline_time];
 }
 
 async function getPicks () {
     const userId = 423253;
-    const [currentGw, currentDeadline] = await getCurrentGw();
-    console.log(currentGw);
-    console.log(currentDeadline)
+    const [currentGw] = await getCurrentGw();
     const user = await getTeamInfo(userId, currentGw);
     return user.picks; // arr of user team objects
 }
 
 async function getPlayerInfo(playerId) {
+    const response = await fetch(`https://fantasy.premierleague.com/api/element-summary/${playerId}/`);
+    const playerFixtures = await response.json();
+    const [nextFixture] = playerFixtures.fixtures
+    console.log(nextFixture.is_home);
+    let playerTeamId;
+    playerTeamId = nextFixture.is_home ? nextFixture.team_h : nextFixture.team_a;
+    console.log(playerTeamId)
+    const teams = await getAllTeams();
+    const playerTeam = teams.find(team => team.id === playerTeamId);
+    console.log(playerTeam.name)
+
+    
     const data = await getGeneralInfo();
-    const player = data.elements.filter(element=> element.id === playerId);
-    console.log(player[0].web_name);
-    return { ...player[0] }; // single player object
+    const player = data.elements.find(element=> element.id === playerId);
+    return player; // single player object
 }
 
 async function loadTeam() {
@@ -42,7 +50,7 @@ async function loadTeam() {
 
 async function getAllTeams() {
     const data = await getGeneralInfo();
-    console.log(data.teams);
+    console.log(data.teams[0].id)
     return data.teams // arr of teams objects
 }
 
@@ -50,10 +58,6 @@ async function getAllPlayers() {
     const data = await getGeneralInfo();
     const players = data.elements;
     return players; //arr of players objects
-}
-
-async function getFixturesOfPlayer() {
-    //todo
 }
 
 getAllPlayers()
