e26b28e3594811142334304cbc13dce65537df07
xdimitaryanev <118223301+xdimitaryanev@users.noreply.github.com>
2023-08-26 23:31:32 +0300

    update

--- fpl-planner/src/App.jsx
+++ fpl-planner/src/App.jsx
@@ -1,13 +1,33 @@
 import './App.css'
+import { getPicks, getPlayerInfo } from './data/api'
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [playerData, setPlayerData] = useState([]);
 
+  useEffect(() => {
+    loadTeam();
+  }, []);
+
+  const loadTeam = async () => {
+    const team = await getPicks();
+    const playerPromises = team.map(element => getPlayerInfo(element.element));
+    const playerData = await Promise.all(playerPromises);
+    setPlayerData(playerData);
+  };
 
   return (
-    <>
-"hi"
-    </>
-  )
+    <div className="App">
+      <h1>Football Team Players</h1>
+      <ul>
+        {playerData.map(player => (
+          <li key={player.id}>
+            {player.web_name} - {player.position}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App
