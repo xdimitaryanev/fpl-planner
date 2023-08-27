f6cd352c1f829a1d0dc813f1a196736c37df223f
xdimitaryanev <118223301+xdimitaryanev@users.noreply.github.com>
2023-08-27 15:57:54 +0300

    switch gw functionality

--- fpl-planner/src/App.jsx
+++ fpl-planner/src/App.jsx
@@ -1,11 +1,42 @@
 import './App.css'
-import { getPicks, getPlayerInfo, getNextGw } from './data/api'
+import { getPicks, getPlayerInfo, getCurrentGw, getFixtureOfPlayer, getNextGw, createUserInfo } from './data/api'
 import React, { useState, useEffect } from 'react';
 
 function App() {
   const [userData, setUserData] = useState([]);
   const [gameWeek, setGameWeek] = useState('');
   const [userId, setUserId] = useState('');
+  const [userInfo, setUserInfo] = useState({});
+
+  const loadUserInfo = async () => {
+    const [currentGw] = await getCurrentGw()
+    const userInfo = await createUserInfo(userId, currentGw);
+    setUserInfo(userInfo)
+    console.log(userInfo)
+    console.log(userInfo.total_points)
+  }
+
+
+  const increaseGameWeek = async (e) => {
+    e.preventDefault();
+    setGameWeek(prevGameWeek => prevGameWeek + 1);
+    const updatedUserData = await loadNextFixtures(userData);
+    setUserData(updatedUserData);
+  };
+
+  const decreaseGameWeek = async (e) => {
+    e.preventDefault();
+    setGameWeek(prevGameWeek => prevGameWeek - 1);
+    const updatedUserData = await loadNextFixtures(userData);
+    setUserData(updatedUserData);
+  };
+
+  const loadNextFixtures = async () => {
+    const playersPromises = userData.map(async player => await getFixtureOfPlayer(player[0].id, gameWeek));
+    const playersArr = await Promise.all(playersPromises);
+    console.log(playersArr)
+    return playersArr;
+  }
 
 
 
@@ -15,9 +46,7 @@ function App() {
     const userData = await Promise.all(userPromises);
     const [nextGw] = await getNextGw();
     setUserData(userData);
-    setGameWeek(nextGw)
-    
-    
+    setGameWeek(nextGw) 
   };
   
 
@@ -34,18 +63,24 @@ function App() {
         <button
         onClick={(e)=> {
           e.preventDefault()
+          loadUserInfo(userId)
           loadTeam(userId)}}>
           Load
         </button>
       </form>
-      <button>-</button>
+      <button
+      onClick={decreaseGameWeek}
+      >-</button>
       <h1>{gameWeek}</h1>
       <button
+      onClick={increaseGameWeek}
+
       >+</button>
+<div>{userInfo.total_points} - {userInfo.overall_rank} </div>
       <ul>
         {userData.map(player => (
           <li key={player[0].id}>
-            {player[0].web_name} - {player[1]} - {player[2]} - {player[3]}
+            {player[0].web_name} - {player[1]} - {player[2]} - {player[3]}, {player[4]}
           </li>
         ))}
       </ul>
--- fpl-planner/src/data/api.js
+++ fpl-planner/src/data/api.js
@@ -1,59 +1,136 @@
 async function getGeneralInfo() {
-    const response = await fetch(`https://fantasy.premierleague.com/api/bootstrap-static/`);
-    return await response.json();
+  const response = await fetch(
+    `https://fantasy.premierleague.com/api/bootstrap-static/`
+  );
+  return await response.json();
 }
 
-async function getTeamInfo(id,gw) {
-    const response = await fetch(`https://fantasy.premierleague.com/api/entry/${id}/event/${gw}/picks/`);
-    return await response.json();
+async function getTeamInfo(userId, gw) {
+  const response = await fetch(
+    `https://fantasy.premierleague.com/api/entry/${userId}/event/${gw}/picks/`
+  );
+  return await response.json();
+}
+
+async function createUserInfo(userId, gw) {
+    const teamInfo = await getTeamInfo(userId,gw)
+    console.log(teamInfo)
+    const userInfoObject = {
+        "total_points": teamInfo.entry_history.total_points,
+        "overall_rank": teamInfo.entry_history.overall_rank,
+        "gameweek_rank": teamInfo.entry_history.rank,
+        "event_transfers": teamInfo.entry_history.event_transfers,
+        "user_teamvalue": teamInfo.entry_history.value,
+        "user_bank": teamInfo.entry_history.bank,
+    }
+    return userInfoObject;
+}
+
+createUserInfo(2,3)
+async function getCurrentGw() {
+  const data = await getGeneralInfo();
+  let events = data.events;
+  const currentGw = events.find((event) => event.is_current === true);
+  return [currentGw.id, currentGw.deadline_time];
 }
 
 async function getNextGw() {
-    const data = await getGeneralInfo();
-    let events = data.events;
-    const currentGw = events.find(event => event.is_current === true);
-    return [currentGw.id, currentGw.deadline_time];
+  const data = await getGeneralInfo();
+  let events = data.events;
+  const currentGw = events.find((event) => event.is_next === true);
+  return [currentGw.id, currentGw.deadline_time];
 }
 
-async function getPicks (userId) {
-    const [nextGw] = await getNextGw();
-    const user = await getTeamInfo(userId, nextGw);
-    return user.picks; // arr of user team objects
+async function getPicks(userId) {
+  const [currentGw] = await getCurrentGw();
+  const user = await getTeamInfo(userId, currentGw);
+  return user.picks; // arr of user team objects
+}
+
+async function getFixtureOfPlayer(playerId, gw) {
+  const response = await fetch(
+    `https://fantasy.premierleague.com/api/element-summary/${playerId}/`
+  );
+  const playerFixtures = await response.json();
+  const teams = await getAllTeams();
+  const fixtureOfPlayer = playerFixtures.fixtures.find(
+    (fixture) => fixture.event === gw
+  );
+  console.log(fixtureOfPlayer);
+  const playerTeamId = fixtureOfPlayer.is_home
+    ? fixtureOfPlayer.team_h
+    : fixtureOfPlayer.team_a;
+  const playerTeam = teams.find((team) => team.id === playerTeamId);
+  const [playerOpponentTeamId, isHome] = fixtureOfPlayer.is_home
+    ? [fixtureOfPlayer.team_a, "A"]
+    : [fixtureOfPlayer.team_h, "H"];
+  const opponentTeam = teams.find((team) => team.id === playerOpponentTeamId);
+  const data = await getGeneralInfo();
+  const player = data.elements.find((element) => element.id === playerId);
+  return [
+    player,
+    playerTeam.name,
+    opponentTeam.name,
+    isHome,
+    player.chance_of_playing_next_round,
+  ];
 }
 
 async function getPlayerInfo(playerId) {
-    const response = await fetch(`https://fantasy.premierleague.com/api/element-summary/${playerId}/`);
-    const playerFixtures = await response.json();
-    const [nextFixture] = playerFixtures.fixtures
-    const playerTeamId = nextFixture.is_home ? nextFixture.team_h : nextFixture.team_a;
-    const [playerNextFixtureTeamId, isHome] = nextFixture.is_home ? [nextFixture.team_a, "A" ] : [nextFixture.team_h, "H"];
-    console.log(isHome)
-    const teams = await getAllTeams();
-    const playerTeam = teams.find(team => team.id === playerTeamId);
-    const oponentTeam = teams.find(team => team.id === playerNextFixtureTeamId);
-    const data = await getGeneralInfo();
-    const player = data.elements.find(element=> element.id === playerId);
-    return [player, playerTeam.name, oponentTeam.name, isHome];
+  const response = await fetch(
+    `https://fantasy.premierleague.com/api/element-summary/${playerId}/`
+  );
+  const playerFixtures = await response.json();
+  const [nextFixture] = playerFixtures.fixtures;
+  const playerTeamId = nextFixture.is_home
+    ? nextFixture.team_h
+    : nextFixture.team_a;
+  const [playerNextFixtureTeamId, isHome] = nextFixture.is_home
+    ? [nextFixture.team_a, "A"]
+    : [nextFixture.team_h, "H"];
+  console.log(isHome);
+  const teams = await getAllTeams();
+  const playerTeam = teams.find((team) => team.id === playerTeamId);
+  const opponentTeam = teams.find(
+    (team) => team.id === playerNextFixtureTeamId
+  );
+  const data = await getGeneralInfo();
+  const player = data.elements.find((element) => element.id === playerId);
+  return [
+    player,
+    playerTeam.name,
+    opponentTeam.name,
+    isHome,
+    player.chance_of_playing_next_round,
+  ];
 }
 
 async function loadTeam(userId) {
-    const team = await getPicks(userId);
-    team.forEach(element => {
-        console.log(element);
-        getPlayerInfo(element.element);
-    });
+  const team = await getPicks(userId);
+  team.forEach((element) => {
+    console.log(element);
+    getPlayerInfo(element.element);
+  });
 }
 
 async function getAllTeams() {
-    const data = await getGeneralInfo();
-    console.log(data.teams[0].id)
-    return data.teams // arr of teams objects
+  const data = await getGeneralInfo();
+  return data.teams; // arr of teams objects
 }
 
 async function getAllPlayers() {
-    const data = await getGeneralInfo();
-    const players = data.elements;
-    return players; //arr of players objects
+  const data = await getGeneralInfo();
+  const players = data.elements;
+  return players; //arr of players objects
 }
 
-export { getGeneralInfo, getTeamInfo, getNextGw, getPicks, getPlayerInfo };
+export {
+  getGeneralInfo,
+  getTeamInfo,
+  getCurrentGw,
+  getNextGw,
+  getPicks,
+  getPlayerInfo,
+  getFixtureOfPlayer,
+  createUserInfo
+};
